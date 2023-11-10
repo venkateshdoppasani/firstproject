d74657a426549b02348408d040c3fb0a4b296e8b
venkateshdoppasani <150415436+venkateshdoppasani@users.noreply.github.com>
2023-11-10 11:10:34 +0530

    Add files via upload
    index.html sample.java

---
+++ 1234.py
@@ -0,0 +1,15 @@
+import pyautogui as spam
+import time
+
+limit=input("no.of messages")
+msg=input("message you want to send")
+
+i=0
+
+time.sleep(5)
+
+while i<int(limit):
+    spam.typewrite(msg)
+    spam.press('enter')
+
+    i+=1
